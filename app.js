85a2da32c1ea0ab0cd862474725c8668db36b4e3
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2020-08-20 18:45:08 +0200

    Setup of server with eslint done
    Server has been set up to run locally on port 3005. Eslint has been setup.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	return res.status(200).json({
+		message: 'Hello world!'
+	});
+});
+
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+// const https = require('https');
+// const fs = require('fs');
+const app = require('./app');
+
+if (process.env.MODE === 'production') {
+	// Certificate
+	// const privateKey = fs.readFileSync('/etc/letsencrypt/live/verdensbestedyr.no/privkey.pem', 'utf8');
+	// const certificate = fs.readFileSync('/etc/letsencrypt/live/verdensbestedyr.no/cert.pem', 'utf8');
+	// const ca = fs.readFileSync('/etc/letsencrypt/live/verdensbestedyr.no/chain.pem', 'utf8');
+
+	// const credentials = {
+	// 	key: privateKey,
+	// 	cert: certificate,
+	// 	ca: ca
+	// };
+
+	// Running on port 3005 because NGINX is listening on port 3005
+	// const port = 3005;
+
+	// const httpsServer = https.createServer(credentials, app);
+
+	// httpsServer.listen(port, () => {
+	// 	console.log('HTTPS Server running on port', port);
+	// });
+} else {
+	const port = 3005;
+
+	const httpServer = http.createServer(app);
+
+	httpServer.listen(port, () => {
+		console.log('HTTP Server running on port', port);
+	});
+}
